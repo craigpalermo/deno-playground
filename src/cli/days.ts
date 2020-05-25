5ed77b91d980f3215239e95b3f5a3f0003667861
Craig Palermo <email@craigp.me>
2020-05-25 11:16:38 -0400

    Add since command to days

--- src/cli/days.ts
+++ src/cli/days.ts
@@ -1,20 +1,40 @@
 import differenceInCalendarDays from "https://deno.land/x/date_fns/differenceInCalendarDays/index.js";
 import { moment } from "https://deno.land/x/moment/moment.ts";
 
+function getTargetDateObj(date) {
+  return moment(date, ["YYYYMMDD", "MMDDYYYY"], false);
+}
+
 /**
  * Returns the number of days from today days a given future date
  *
  * Usage:
  *   # To get the days until Christmas 2026
- *   days "2026.12.25"
+ *   days until "2026.12.25"
  *
  * @param targetDate
  */
 function until(targetDate: string): number {
-  const targetDateObj = moment(targetDate, ["YYYYMMDD", "MMDDYYYY"], false);
+  const targetDateObj = getTargetDateObj(targetDate);
   const today: Date = new Date();
   // TODO error handling for targetDate before today
   return -1 * differenceInCalendarDays(today, targetDateObj.toDate());
+}
+
+/**
+ * Returns the number of days since a past date
+ *
+ * Usage:
+ *   # To get the days since January 1st, 2020
+ *   days since "2020.01.01"
+ *
+ * @param targetDate
+ */
+function since(targetDate: string): number {
+  const targetDateObj = getTargetDateObj(targetDate);
+  const today: Date = new Date();
+  // TODO error handling for targetDate after today
+  return 1 * differenceInCalendarDays(today, targetDateObj.toDate());
 }
 
 // TODO add help command to list out available commands
@@ -28,10 +48,12 @@ if (dateInput && command) {
 
   if (command === "until") {
     processor = until;
+  } else if (command === "since") {
+    processor = since;
   }
 
   if (typeof processor === "function") {
-    console.log(`${processor(dateInput)} days until ${dateInput}`);
+    console.log(`${processor(dateInput)} days ${command} ${dateInput}`);
   } else {
     console.log(`You need to enter a command, e.g. "until 2025-03-12"`);
   }
